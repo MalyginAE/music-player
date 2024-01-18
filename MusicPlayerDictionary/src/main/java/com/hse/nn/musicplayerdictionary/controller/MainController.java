4aa1d987f677994a98c053c43e8eae4c1e912673
andre <viaz2002@mail.ru>
2024-01-18 21:52:16 +0300

    add bridge between lombok and mapstruct

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
@@ -29,7 +29,7 @@ public class MainController {
         return ResponseEntity.ok(musicTickets);
     }
 
-    @PostMapping("/save")
+    @GetMapping("/save")
     public ResponseEntity<List<MusicTicket>> save(@RequestBody SaveTicketRequest request) {
         log.debug("Got request with body: {}", request);
 
