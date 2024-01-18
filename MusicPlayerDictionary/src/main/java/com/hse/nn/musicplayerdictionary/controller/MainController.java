1909ecc59564ade28a70f6c61f750ccc6bc790d5
andre <viaz2002@mail.ru>
2024-01-18 21:35:46 +0300

    add bridge between lombok and mapstruct

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
@@ -5,6 +5,7 @@ import com.hse.nn.musicplayerdictionary.model.dto.request.SaveTicketRequest;
 import com.hse.nn.musicplayerdictionary.repository.MusicTicketRepository;
 import com.hse.nn.musicplayerdictionary.service.TicketServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,24 +17,29 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/hse/api/v1/music-player-dictionary")
 @RequiredArgsConstructor
+@Slf4j
 public class MainController {
     private final MusicTicketRepository musicTicketRepository;
     private final TicketServiceImpl ticketService;
 
     @GetMapping("/search")
     public ResponseEntity<List<MusicTicket>> get(@RequestParam("trackTitle") String title) {
+        log.debug("Got request with title: {}", title);
         List<MusicTicket> musicTickets = musicTicketRepository.findByTrackTitleContaining(title);
         return ResponseEntity.ok(musicTickets);
     }
 
     @PostMapping("/save")
     public ResponseEntity<List<MusicTicket>> save(@RequestBody SaveTicketRequest request) {
+        log.debug("Got request with body: {}", request);
+
         List<MusicTicket> saved = ticketService.saveBulk(request);
         return ResponseEntity.ok(saved);
     }
 
     @GetMapping(value = "/music/{id}", produces = "audio/mp3")
     public @ResponseBody byte[] getImage(@PathVariable String id) throws IOException {
+        log.debug("Got request with id: {}", id);
         InputStream in = getClass().getResourceAsStream("/static/" + id + ".mp3");
         return Objects.requireNonNull(in).readAllBytes();
     }
