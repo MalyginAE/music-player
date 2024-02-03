bb712880931ef7dd29ad07f53313e9839ef927f5
andre <viaz2002@mail.ru>
2024-02-03 22:50:39 +0300

    add bridge between lombok and mapstruct

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/dto/request/SaveTicketRequest.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/dto/request/SaveTicketRequest.java
@@ -3,9 +3,13 @@ package com.hse.nn.musicplayerdictionary.model.dto.request;
 import lombok.Data;
 
 import java.util.List;
+
 @Data
 public class SaveTicketRequest {
     private String trackTitle;
+    private String trackId;
+    private String trackCoverId;
+    private String trackAuthor;
 
     private List<SaveTicketRequest> tickets;
 }
