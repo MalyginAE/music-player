6bfce97acba44239a72a9893b4683241ff76a2da
andre <viaz2002@mail.ru>
2024-04-13 22:02:08 +0300

    add swager

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/controller/MainController.java
@@ -4,6 +4,8 @@ import com.hse.nn.musicplayerdictionary.model.MusicTicket;
 import com.hse.nn.musicplayerdictionary.model.dto.request.SaveTicketRequest;
 import com.hse.nn.musicplayerdictionary.model.dto.request.response.MusicTicketResponse;
 import com.hse.nn.musicplayerdictionary.service.TicketServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +24,14 @@ public class MainController {
     private final TicketServiceImpl ticketService;
 
     @GetMapping("/search")
+    @Operation(summary = "Поиск трека по заголовку")
     public ResponseEntity<List<MusicTicketResponse>> get(@RequestParam("trackTitle") String title) {
         log.debug("Got request with title: {}", title);
         List<MusicTicketResponse> musicTickets = ticketService.getMusicTickets(title);
         return ResponseEntity.ok(musicTickets);
     }
 
+    @Operation(summary = "Сохранение трека в бд")
     @PostMapping("/save")
     public ResponseEntity<List<MusicTicket>> save(@RequestBody SaveTicketRequest request) {
         log.debug("Got request with body: {}", request);
@@ -35,6 +39,7 @@ public class MainController {
         return ResponseEntity.ok(saved);
     }
 
+    @Operation(summary = "Получение популярной музыки", description = "Вся музыка на моках и не является актуальной.")
     @GetMapping("/music/popular")
     public ResponseEntity<List<MusicTicketResponse>> getPopularMusic() {
         log.debug("Got request to get popular info");
@@ -42,6 +47,7 @@ public class MainController {
         return ResponseEntity.ok(tickets);
     }
 
+    @Operation(summary = "Недавно выпущенная музыка")
     @GetMapping("/music/new")
     public ResponseEntity<List<MusicTicketResponse>> getNewMusic() {
         log.debug("Got request to get new info");
@@ -49,6 +55,7 @@ public class MainController {
         return ResponseEntity.ok(tickets);
     }
 
+    @Operation(summary = "Получение музыки по id")
     @GetMapping(value = "/music/{id}", produces = "audio/mp3")
     public @ResponseBody byte[] getMusic(@PathVariable String id) throws IOException {
         log.debug("Got request with id: {}", id);
@@ -56,6 +63,7 @@ public class MainController {
         return Objects.requireNonNull(in).readAllBytes();
     }
 
+    @Operation(summary = "Получение изображения по id")
     @GetMapping(value = "/image/{id}", produces = "image/png")
     public @ResponseBody byte[] getImage(@PathVariable String id) throws IOException {
         log.debug("Got request with id: {}", id);
@@ -63,10 +71,11 @@ public class MainController {
         return Objects.requireNonNull(in).readAllBytes();
     }
 
-    @GetMapping(value = "/image/{id}", produces = "image/png")
-    public @ResponseBody byte[] saveLike(@PathVariable String id) throws IOException {
-        log.debug("Got request with id: {}", id);
-        InputStream in = getClass().getResourceAsStream("/static/image/" + id);
-        return Objects.requireNonNull(in).readAllBytes();
-    }
+
+//    @GetMapping(value = "/image/{id}", produces = "image/png")
+//    public @ResponseBody byte[] saveLike(@PathVariable String id) throws IOException {
+//        log.debug("Got request with id: {}", id);
+//        InputStream in = getClass().getResourceAsStream("/static/image/" + id);
+//        return Objects.requireNonNull(in).readAllBytes();
+//    }
 }
--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/dto/request/SaveTicketRequest.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/dto/request/SaveTicketRequest.java
@@ -1,14 +1,19 @@
 package com.hse.nn.musicplayerdictionary.model.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SaveTicketRequest {
+    @Schema(name = "Заголовок тикета", description = "По нему можно производить поиск")
     private String trackTitle;
+    @Schema(name = "Присвоится автоматически")
     private String trackId;
+    @Schema(name = "")
     private String trackCoverId;
+    @Schema(name = "Автор произведения")
     private String trackAuthor;
 
     private List<SaveTicketRequest> tickets;
