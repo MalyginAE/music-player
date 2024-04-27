a863b4c9ae94234e8a0d493b853a967939bcb585
andre <viaz2002@mail.ru>
2024-04-27 11:19:28 +0300

    add custom redirect url provider

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/entity/User.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/model/entity/User.java
@@ -3,20 +3,28 @@ package com.hse.nn.musicplayerdictionary.model.entity;
 import com.hse.nn.musicplayerdictionary.model.Provider;
 import com.hse.nn.musicplayerdictionary.model.Role;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "userName", nullable = false)
+    @Column(name = "username", nullable = false)
     private String userName;
 
-    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", nullable = false)
     private Role role;
 
     @Column(name = "password", nullable = false)
