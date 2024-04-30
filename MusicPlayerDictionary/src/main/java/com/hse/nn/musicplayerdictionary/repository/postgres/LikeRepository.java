5e24d62ede5717bacc354734e7ac1e3ea8c1a707
andre <viaz2002@mail.ru>
2024-04-30 23:27:11 +0300

    add new tables

--- MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/repository/postgres/LikeRepository.java
+++ MusicPlayerDictionary/src/main/java/com/hse/nn/musicplayerdictionary/repository/postgres/LikeRepository.java
@@ -3,5 +3,5 @@ package com.hse.nn.musicplayerdictionary.repository.postgres;
 import com.hse.nn.musicplayerdictionary.model.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Long, Like> {
+public interface LikeRepository extends JpaRepository<Like,Long> {
 }
