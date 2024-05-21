137c39eb1ba327474af5d854238bf1578d43e3ef
andre <viaz2002@mail.ru>
2024-05-21 16:44:04 +0300

    add tests

--- MusicPlayerDictionary/src/test/java/com/hse/nn/musicplayerdictionary/controller/LikeControllerTest.java
+++ MusicPlayerDictionary/src/test/java/com/hse/nn/musicplayerdictionary/controller/LikeControllerTest.java
@@ -1,64 +1,68 @@
 package com.hse.nn.musicplayerdictionary.controller;
 
-import com.hse.nn.musicplayerdictionary.model.Role;
-import com.hse.nn.musicplayerdictionary.model.entity.User;
-import com.hse.nn.musicplayerdictionary.repository.postgres.UserRepository;
 import com.hse.nn.musicplayerdictionary.service.LikeService;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
+
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 class LikeControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
 
     @Mock
-    private UserRepository userRepository;
+    private LikeService likeService;
+
+    private LikeController likeController;
+
+    @BeforeEach
+    void setup() {
+        likeController = new LikeController(likeService);
+    }
 
 
-    @WithMockUser()
     @SneakyThrows
     @Test
     void like() {
-//        when(userRepository.findUserByUserName("")).thenReturn(Optional.of(new User()));
-//        when(likeService.saveLike("1"))
-//        mockMvc.perform(post("/hse/api/v1/music-player-dictionary/likes/like")
-//                        .queryParam("trackId", "1"));
-//                .andExpect(status().is(200))
-//                .andExpect(content().contentType("application/json"));
+        //when
+        ResponseEntity responseEntity = likeController.like("1");
+        //then
+        Assertions.assertEquals("200 OK",responseEntity.getStatusCode().toString());
     }
 
     @Test
     void resetLike() {
+        //when
+        ResponseEntity responseEntity = likeController.resetLike("1");
+        //then
+        Assertions.assertEquals("200 OK",responseEntity.getStatusCode().toString());
     }
 
     @Test
     void checkLike() {
+        //when
+        ResponseEntity responseEntity = likeController.checkLike("1");
+        //then
+        Assertions.assertEquals("200 OK",responseEntity.getStatusCode().toString());
     }
 
     @Test
     void likes() {
+        //given
+        when(likeService.getMusicTickets()).thenReturn(List.of());
+        //when
+        ResponseEntity responseEntity = likeController.likes();
+        //then
+        Assertions.assertEquals("200 OK",responseEntity.getStatusCode().toString());
+        Assertions.assertNotNull(responseEntity.getBody());
     }
 }
